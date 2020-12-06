06e5c6828c55e5d787720d35c15ba152818e41d9
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2020-12-05 18:19:23 -0600

    actualizacion gui

--- BazarNavideño/MainWindow.xaml.cs
+++ BazarNavideño/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,30 +21,69 @@ namespace BazarNavideño
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<Producto> stock = new List<Producto>
-        {
-            new Producto
-            {
-                Nombre = "Paquetes de Tines",
-                Cantidad = 200
-            },
-
-            new Producto
-            {
-                Nombre = "Suéteres para hombre",
-                Cantidad = 50
-            },
-
-            new Producto
-            {
-                Nombre = "Suéteres para mujer",
-                Cantidad = 150
-            }
-        };
+        private Producto tines;
+        private Producto sueteresHombre;
+        private Producto sueteresMujer;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            tines = new Producto
+            {
+                Cantidad = 200,
+                Precio = 250.00f
+            };
+
+            sueteresHombre = new Producto
+            {
+                Cantidad = 50,
+                Precio = 520.00f
+            };
+
+            sueteresMujer = new Producto
+            {
+                Cantidad = 150,
+                Precio = 450.00f
+            };
+        }
+
+        private void EsTextoNumero(object sender, TextCompositionEventArgs e)
+        {
+            Regex regexNumero = new Regex(@"\d");
+            bool aceptaTexto = true;
+
+            if (regexNumero.IsMatch(e.Text))
+            {
+                aceptaTexto = false;               
+            }
+
+            e.Handled = aceptaTexto;
+        }
+
+        private void RealizarOtroPedido(object sender, RoutedEventArgs e)
+        {
+            nombreClienteText.Text = "";
+            direccionClienteText.Text = "";
+            cantidadSetTinesText.Text = "0";
+            cantidadSueterHombreText.Text = "0";
+            cantidadSueterMujerText.Text = "0";
+            codigoDescuentoText.Text = "";
+            subTotalText.Text = "";
+            descuentoText.Text = "";
+            ivaText.Text = "";
+            totalText.Text = "";
+            fechaEntregaText.Text = "";
+        }
+
+        private void RealizarPedidoBoton(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Salir(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 
@@ -51,5 +91,6 @@ namespace BazarNavideño
     {
         public string Nombre { get; set; }
         public int Cantidad { get; set; }
+        public float Precio { get; set; }
     }
 }
