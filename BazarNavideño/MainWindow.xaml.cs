cdfd4a3c7e804e067881031c9af1ae3a65451caa
Pklove3459 <61339804+Pklove3459@users.noreply.github.com>
2021-01-02 02:35:22 -0600

    bazar

--- BazarNavideño/MainWindow.xaml.cs
+++ BazarNavideño/MainWindow.xaml.cs
@@ -78,7 +78,157 @@ namespace BazarNavideño
 
         private void RealizarPedidoBoton(object sender, RoutedEventArgs e)
         {
+            if (!string.IsNullOrWhiteSpace(nombreClienteText.Text) || !string.IsNullOrWhiteSpace(direccionClienteText.Text))
+            {
+                if (Convert.ToInt32(cantidadSetTinesText.Text) > 0 || Convert.ToInt32(cantidadSueterHombreText.Text) > 0 || Convert.ToInt32(cantidadSueterMujerText.Text) > 0)
+                {
 
+                    double precioTines = 250 * (Convert.ToDouble(cantidadSetTinesText.Text as string));
+                    double precioSueteresHombre = 520 * (Convert.ToDouble(cantidadSueterHombreText.Text as string));
+                    double precioSueteresMujer = 450 * (Convert.ToDouble(cantidadSueterMujerText.Text as string));
+
+                    if (precioTines > 0)
+                    {
+                        if (Convert.ToInt32(cantidadSetTinesText.Text) <= tines.Cantidad || Convert.ToInt32(cantidadSueterHombreText.Text) <= sueteresHombre.Cantidad || Convert.ToInt32(cantidadSueterMujerText.Text) <= sueteresMujer.Cantidad)
+                        {
+                            tines.Cantidad = tines.Cantidad - Convert.ToInt32(cantidadSetTinesText.Text);
+                        }
+                        else
+                        {
+                            precioTines = 0;
+                            precioSueteresHombre = 0;
+                            precioSueteresMujer = 0;
+                            if (Convert.ToInt32(cantidadSetTinesText.Text) == 0)
+                            {
+                                MessageBox.Show("Se nos ha agotado el inventario. Acutalmente se encuentra asi : \nTines: "+ tines.Cantidad + "\n Sueteres Hombre:"+ sueteresHombre.Cantidad + "\n Suteres Mujer : "+ sueteresMujer.Cantidad );
+                                return;
+                            }
+                            else
+                            {
+                                MessageBox.Show("La cantidad de tines solicitada rebasa el numero de producto que tenemos, favor de igualarlo o reducirlo a " + tines.Cantidad);
+                                return;
+                            }
+                        }
+                    }
+
+                    if (precioSueteresHombre > 0)
+                    {
+                        if (Convert.ToInt32(cantidadSueterHombreText.Text) <= sueteresHombre.Cantidad || Convert.ToInt32(cantidadSueterMujerText.Text) <= sueteresMujer.Cantidad)
+                        {
+                            sueteresHombre.Cantidad = sueteresHombre.Cantidad - Convert.ToInt32(cantidadSueterHombreText.Text);
+                        }
+                        else
+                        {
+                            precioTines = 0;
+                            precioSueteresHombre = 0;
+                            precioSueteresMujer = 0;
+                            if (Convert.ToInt32(cantidadSueterHombreText.Text) == 0)
+                            {
+                                MessageBox.Show("Se nos ha agotado el inventario. Acutalmente se encuentra asi : \nTines: " + tines.Cantidad + "\n Sueteres Hombre:" + sueteresHombre.Cantidad + "\n Suteres Mujer : " + sueteresMujer.Cantidad);
+                                return;
+                            }
+                            else
+                            {
+                                MessageBox.Show("La cantidad de sueteres solicitada rebasa el numero de producto que tenemos, favor de igualarlo o reducirlo a " + sueteresHombre.Cantidad);
+                                return;
+                            }
+                        }
+                    }
+
+                    if (precioSueteresMujer > 0)
+                    {
+                        if (Convert.ToInt32(cantidadSueterMujerText.Text) <= sueteresMujer.Cantidad)
+                        {
+                            sueteresMujer.Cantidad = sueteresMujer.Cantidad - Convert.ToInt32(cantidadSueterMujerText.Text);
+                        }
+                        else
+                        {
+                            precioTines = 0;
+                            precioSueteresHombre = 0;
+                            precioSueteresMujer = 0;
+                            if (Convert.ToInt32(cantidadSueterMujerText) == 0)
+                            {
+                                MessageBox.Show("Los Sueteres se han agotado");
+                                return;
+                            }
+                            else
+                            {
+                                MessageBox.Show("La cantidad de sueteres solicitada rebasa el numero de producto que tenemos, favor de igualarlo o reducirlo a " + sueteresMujer.Cantidad);
+                                return;
+                            }
+                        }
+                    }
+
+                    double subtotal = precioSueteresHombre + precioSueteresMujer + precioTines;
+                 
+                    if (!string.IsNullOrWhiteSpace(descuentoText.Text))
+                    {
+                        string descuentoString = Convert.ToString(codigoDescuentoText.Text);
+                        double descuentoDouble = 0;
+                        switch (descuentoString)
+                        {
+                            case "dasher":
+                                descuentoDouble =  0.05 * subtotal;
+                                subtotal = subtotal - descuentoDouble;
+                                descuentoText.Text = Convert.ToString(descuentoDouble);
+                                subTotalText.Text = Convert.ToString(subtotal);
+                                break;
+                            case "dancer":
+                                descuentoDouble = 0.10 * subtotal;
+                                subtotal = subtotal - descuentoDouble;
+                                descuentoText.Text = Convert.ToString(descuentoDouble);
+                                subTotalText.Text = Convert.ToString(subtotal);
+                                break;
+                            case "prancer":
+                                descuentoDouble = 0.15 * subtotal;
+                                subtotal = subtotal - descuentoDouble;
+                                descuentoText.Text = Convert.ToString(descuentoDouble);
+                                subTotalText.Text = Convert.ToString(subtotal);
+                                break;
+                            case "rudolph":
+                                descuentoDouble = 0.20 * subtotal;
+                                subtotal = subtotal - descuentoDouble;
+                                descuentoText.Text = Convert.ToString(descuentoDouble);
+                                subTotalText.Text = Convert.ToString(subtotal);
+                                break;
+                            default:
+                                MessageBox.Show("Ingreso un descuento no valido");
+                                descuentoText.Text = Convert.ToString(descuentoDouble);
+                                subTotalText.Text = Convert.ToString(subtotal);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        descuentoText.Text = Convert.ToString(0);
+                        subTotalText.Text = Convert.ToString(subtotal);
+                    }
+
+
+                    double iva = subtotal * 0.16;
+                    ivaText.Text = Convert.ToString(iva);
+                    totalText.Text = Convert.ToString(subtotal + iva);
+                    DayOfWeek fecha = DateTime.Now.DayOfWeek;
+
+                    if (fecha == DayOfWeek.Friday || fecha == DayOfWeek.Saturday || fecha == DayOfWeek.Sunday)
+                    {
+                        fechaEntregaText.Text = "Martes";
+                    }
+                    else
+                    {
+                        fechaEntregaText.Text = "Sabado";
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Favor de ingresar minimo 1 producto de los mostrados");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Favor de ingresar sus datos de nombre y direccion");
+            }
         }
 
         private void Salir(object sender, RoutedEventArgs e)
